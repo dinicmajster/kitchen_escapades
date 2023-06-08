a4bf57cdd74f6ed5c2514d7b8f734bba06189876
igor dinic <dinicigor84@gmail.com>
2023-06-08 22:00:05 +0200

    added some styling on Tom Select on show & index page

--- db/schema.rb
+++ db/schema.rb
@@ -51,15 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_174943) do
     t.index ["kitchen_id"], name: "index_feature_kitchens_on_kitchen_id"
   end
 
-  create_table "feature_tags", force: :cascade do |t|
-    t.bigint "feature_id", null: false
-    t.bigint "tag_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["feature_id"], name: "index_feature_tags_on_feature_id"
-    t.index ["tag_id"], name: "index_feature_tags_on_tag_id"
-  end
-
   create_table "features", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -83,12 +74,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_174943) do
     t.index ["user_id"], name: "index_kitchens_on_user_id"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -107,7 +92,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_174943) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "feature_kitchens", "features"
   add_foreign_key "feature_kitchens", "kitchens"
-  add_foreign_key "feature_tags", "features"
-  add_foreign_key "feature_tags", "tags"
   add_foreign_key "kitchens", "users"
 end
