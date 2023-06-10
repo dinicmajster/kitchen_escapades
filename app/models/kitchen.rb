384117ff31141a0b2d026917d9743a3da3de6ed6
igor dinic <dinicigor84@gmail.com>
2023-06-10 16:10:02 +0200

    money gem setup finished

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -1,4 +1,5 @@
 class Kitchen < ApplicationRecord
+  monetize :price_cents
   has_many_attached :photos
   has_many :feature_kitchens, dependent: :destroy
   has_many :features, through: :feature_kitchens
