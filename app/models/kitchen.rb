788288e96b08bb255e0ef82993e984ed9e671cfd
igor dinic <dinicigor84@gmail.com>
2023-06-10 17:48:35 +0200

    bookings dependet destroy

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -3,7 +3,7 @@ class Kitchen < ApplicationRecord
   has_many_attached :photos
   has_many :feature_kitchens, dependent: :destroy
   has_many :features, through: :feature_kitchens
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   belongs_to :user
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
